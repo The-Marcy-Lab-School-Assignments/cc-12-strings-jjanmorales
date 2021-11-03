1333c51eebe84a5904d8e85670dbfe1d56fd81e7
Jan Morales <moralesjan40@gmail.com>
2021-11-03 14:46:00 +0000

    Completed code challenge

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //write your code here
+const reverse = string => {
+  let newString = " "
+  for(let i = string.length - 1; i >= 0; i--){
+    newString += string[i]
+  }return newString;
+}
+
+console.log(reverse("jan morales"));
